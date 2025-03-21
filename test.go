837cf163e945f71d47147d4e7b99f3b33262179d
victorpoliushko <victor.poliushko@gmail.com>
2025-03-21 16:35:31 +0200

    slice, loop

--- test.go
+++ test.go
@@ -10,6 +10,8 @@ func main() {
 	testTwo()
 }
 
+// Interface
+
 type Book struct {
 	Title string
 	Author string
@@ -25,6 +27,54 @@ func (c Count) String() string {
 	return strconv.Itoa(int(c))
 }
 
+
+// Function
+
 func test() {
 	fmt.Println("test")
 }
+
+func printParam(s string) {
+	fmt.Printf(`Printed: %s`, s)
+}
+
+func callFunc(func (int) int) {
+	
+}
+
+
+// Slice
+
+var newSlice []int
+
+// possible to create this way only in functions
+
+func funcForSlices() {
+	newSlice := []string{"First value", "Second value", "Third value"}
+	makeNewSlice := make([]int, 5)
+	makeNewSliceWithCapacity := make([]int, 5, 3)
+
+	arr := [5]int{1, 2, 3, 4, 5}
+	arrSlice := arr[1:4]
+
+	newSlice = append(newSlice, )
+
+	matrixSlice := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+}
+
+
+// Loop (slice)
+
+func loopFunction() {
+	messages := []string{"first message", "Second", "Third message"}
+	for index, value := range messages {
+		fmt.Println(index)
+		printParam()
+	}
+}
+
+
+
