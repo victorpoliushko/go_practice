26aff30edd6be4462cc950f5943a4f2bb97292ce
victorpoliushko <victor.poliushko@gmail.com>
2025-04-29 22:41:39 +0300

    go channels

--- test.go
+++ test.go
@@ -457,3 +457,27 @@ func sendIsOld(isOldChan chan <- bool, emails [3]channelsEmail) {
 		isOldChan <- false
 	}
 }
+
+
+// Channels 2
+
+func waitForDBs(numDBs int, dbChan chan struct{}) {
+	for i := 0; i < numDBs; i++ {
+		<-dbChan
+	}
+}
+
+func getDBsChannel(numDBs int) (chan struct{}, *int) {
+	count := 0
+	ch := make(chan struct{})
+
+	go func() {
+		for i := 0; i < numDBs; i++ {
+			ch <- struct{}{}
+			fmt.Printf("Database %v is online\n", i+1)
+			count++
+		}
+	}()
+
+	return ch, &count
+}
