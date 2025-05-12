569c25633e345304cdb425d650a1ce941be9cccb
victorpoliushko <victor.poliushko@gmail.com>
2025-05-12 20:20:12 +0300

    constraints

--- test.go
+++ test.go
@@ -674,3 +674,57 @@ func getLast[T any](s []T) T {
 		return lastVal
 	}
 }
+
+// Generics: Constrains
+
+func chargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]T, float64, error) {
+	var zeroVal []T
+	if balance < newItem.GetCost() {
+		return zeroVal, .0, errors.New("insufficient funds")
+	}
+	newSlice := []T{}
+	newSlice = append(oldItems, newItem)
+	newBalance := balance - newItem.GetCost()
+	return newSlice, newBalance, nil
+}
+
+// don't edit below this line
+
+type lineItem interface {
+	GetCost() float64
+	GetName() string
+}
+
+type subscription struct {
+	userEmail string
+	startDate time.Time
+	interval  string
+}
+
+func (s subscription) GetName() string {
+	return fmt.Sprintf("%s subscription", s.interval)
+}
+
+func (s subscription) GetCost() float64 {
+	if s.interval == "monthly" {
+		return 25.00
+	}
+	if s.interval == "yearly" {
+		return 250.00
+	}
+	return 0.0
+}
+
+type oneTimeUsagePlan struct {
+	userEmail        string
+	numEmailsAllowed int
+}
+
+func (otup oneTimeUsagePlan) GetName() string {
+	return fmt.Sprintf("one time usage plan with %v emails", otup.numEmailsAllowed)
+}
+
+func (otup oneTimeUsagePlan) GetCost() float64 {
+	const costPerEmail = 0.03
+	return float64(otup.numEmailsAllowed) * costPerEmail
+}
